7edabc689b2060506a129ed2f569b8d89a08b198
Jyn <85330965+janeblower@users.noreply.github.com>
2022-10-01 13:51:23 +0700

    update ./services

--- services/stat.service.js
+++ services/stat.service.js
@@ -6,7 +6,7 @@ export default {
 
         get: {
             async handler(ctx) {
-                const [position, ezwow, cookies, characters, races, classes] = await Promise.all([
+                const [position, ezwow, cookies, characters, racesValue, classesValue] = await Promise.all([
                     this.broker.cacher.get('start.point'),
                     ctx.call('ezwow.stat'),
                     ctx.call('cookie.count'),
@@ -14,6 +14,22 @@ export default {
                     ctx.call('character.races'),
                     ctx.call('character.classes')
                 ])
+
+                const races = [];
+                const racesName = ["Человек", "Дворф", "Найтэльф", "Гном", "Дреней", "Орк", "Нежить", "Таурен", "Тролль", "Бладэльф"];
+
+                for (let i = 0; i < racesName.length; i++) {
+                    races.push({id: i, name: racesName[i], value: racesValue[i], width: 0});
+                }
+
+
+                const classes = [];
+                const classesName = ["Охотник", "Чернокнижник", "Жрец", "Паладин", "Маг", "Разбойник", "Друид", "Шаман", "Воин", "ДК"];
+
+                for (let i = 0; i < classesName.length; i++) {
+                    classes.push({id: i, name: classesName[i], value: classesValue[i], width: 0});
+                }
+
                 return {
                     position, ezwow, cookies, characters, races, classes
                 }
