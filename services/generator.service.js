ef378e71979d9705dd12d11ad6b07cee89d23148
Jyn <85330965+janeblower@users.noreply.github.com>
2022-12-21 00:04:52 +0700

    cleanup

--- services/generator.service.js
+++ services/generator.service.js
@@ -4,6 +4,7 @@ import { promises as fs } from 'fs'
 import _ from 'lodash'
 import Zip from 'adm-zip'
 
+
 export default {
 
     name: 'generator',
@@ -44,35 +45,84 @@ export default {
             }
         },
 
-        sqlzip: {
-            params: {
-                filename: 'string|max:256|optional|default:IsengardArmory.sql.zip'
-            },
+        addon80: {
             async handler(ctx) {
                 ctx.meta.$responseType = 'application/zip'
                 ctx.meta.$responseHeaders = {
-                    'Content-Disposition': `attachment; filename="${ctx.params.filename}"`
+                    'Content-Disposition': `attachment; filename="IsengardArmory.zip"`
                 }
-                const characters = await ctx.call('character.find')
-                const zip = new Zip()
-                zip.addFile('IsengardArmory.sql', Buffer.from(this.SQLTemplate({characters}), 'utf8'))
-                return zip.toBuffer()
-            }
-        },
+                const characters = await ctx.call('character.find', {sort: 'login'})
+                let characters80 = []
+                for (var i = 0; i < characters.length; i++)
+                {
+                    if (characters[i].lvl == 80)
+                    {
+                        characters80.push(characters[i]);
+                    }
+                }                
+                //сортировка по аккаунту
+                const sorted = []
+                let login = ''
+                let persons = []
 
-        toc: {
-            params: {
-                filename: 'string|max:256|optional|default:IsengardArmory.toc'
-            },
-            async handler(ctx) {
-                ctx.meta.$responseType = 'application/octet-stream'
-                ctx.meta.$responseHeaders = {
-                    'Content-Disposition': `attachment; filename="${ctx.params.filename}"`
+                _.map(characters80, character => {
+                    if (character.login == login)
+                        persons.push(character)
+                    else {
+                        if (persons.length) {
+                                sorted.push({
+                                    login,
+                                    persons
+                                })
+                            persons = []
+                        }
+                        login = character.login
+                        persons.push(character)
+                    }
+                })
+                
+                if (persons.length) {
+                    sorted.push({
+                        login,
+                        persons
+                    })
                 }
-                const stream = new Readable()
-                stream.push(this.TOCTemplate())
-                stream.push(null)
-                return stream
+
+                const zip = new Zip()
+
+                let index = 0;
+                let sliced = []
+                for (index; index * 40000 < sorted.length; index++) {
+                    sliced = sorted.slice(index * 40000, (index + 1) * 40000)
+                    zip.addFile('IsengardArmory/DB' + index + '.lua', Buffer.from(this.LUADBTemplate({
+                        sliced,
+                        index
+                    }), 'utf8'))
+                }
+
+                zip.addFile('IsengardArmory/IsengardArmory.toc', Buffer.from(this.TOCTemplate({
+                    index
+                    }), 'utf8'))
+
+                zip.addFile('IsengardArmory/IsengardArmory.lua', Buffer.from(this.LUATemplate({
+                    date: new Date().toLocaleString('ru'),
+                    accounts: sorted.length,
+                    characters: characters80.length,
+                    index
+                }), 'utf8'))
+                async function createZipArchive() {
+                    try {
+                      const outputFile = "/root/ez-parse-server-master/public/files/test.zip";
+                      zip.writeZip(outputFile);
+                      console.log(`Created ${outputFile} successfully`);
+                    } catch (e) {
+                      console.log(`Something went wrong. ${e}`);
+                    }
+                  }
+                  
+                createZipArchive();
+
+                return zip.toBuffer()
             }
         },
 
@@ -102,19 +152,22 @@ export default {
                         persons.push(character)
                     }
                 })
+                
                 if (persons.length) {
                     sorted.push({
                         login,
                         persons
                     })
                 }
+
                 const zip = new Zip()
 
                 let index = 0;
-
+                let sliced = []
                 for (index; index * 40000 < sorted.length; index++) {
+                    sliced = sorted.slice(index * 40000, (index + 1) * 40000)
                     zip.addFile('IsengardArmory/DB' + index + '.lua', Buffer.from(this.LUADBTemplate({
-                        sorted,
+                        sliced,
                         index
                     }), 'utf8'))
                 }
@@ -131,53 +184,6 @@ export default {
                 }), 'utf8'))
 
                 return zip.toBuffer()
-            }
-        },
-
-        lua: {
-            params: {
-                filename: 'string|max:256|optional|default:IsengardArmory.lua'
-            },
-            async handler(ctx) {
-                ctx.meta.$responseType = 'application/octet-stream'
-                ctx.meta.$responseHeaders = {
-                    'Content-Disposition': 'attachment; filename="IsengardArmory.lua"'
-                }
-                const characters = await ctx.call('character.find', {sort: 'login'})
-                //сортировка по аккаунту
-                const sorted = []
-                let login = ''
-                let persons = []
-                _.map(characters, character => {
-                    if (character.login == login)
-                        persons.push(character)
-                    else {
-                        if (persons.length) {
-                            sorted.push({
-                                login,
-                                persons
-                            })
-                            persons = []
-                        }
-                        login = character.login
-                        persons.push(character)
-                    }
-                })
-                if (persons.length) {
-                    sorted.push({
-                        login,
-                        persons
-                    })
-                }
-                const stream = new Readable()
-                stream.push(this.LUATemplate({
-                    sorted,
-                    date: new Date().toLocaleString('ru'),
-                    accounts: sorted.length,
-                    characters: characters.length
-                }))
-                stream.push(null)
-                return stream
             }
         }
 
